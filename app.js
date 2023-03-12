4ae5651bf156cc45dcabbdf8de73cce5241d2ef3
Atharva <atharvasenbarai@gmail.com>
2023-03-12 14:53:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 const { application } = require("express");
 const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "";
 const aboutContent = "Hello There👋, This is Atharva, this is my personal blog WebApp. I made it because I am very interested in doing Coding. In This Blog I mostly tags or Represent useful blogs which is needed by most of the people from Around The World for which they always try to Find Answer on Search Engines. I always try to cover Important And Useful Things. If u have any Suggestions related to Blogs or any query so PLS contact me. If i like your Suggestion so I will cover that on my Blog, so PLS don't forgot to Give your name. Thanks For Viewing my About page😊.";
 const contactContent = "Thanks for visiting Contact Us Page contact me on email- contactme.atharvasen@gmail.com";
 
